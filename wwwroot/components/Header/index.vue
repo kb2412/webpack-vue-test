8add63205756b9117de1ca81890cf5008b9adc16
朱云龙 <zhuyunlong@51tz.com>
2018-08-14 23:02:16 +0800

    modify index.vue

--- wwwroot/components/Header/index.vue
+++ wwwroot/components/Header/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<header class="header-vue">
 		<head-bar>
-			<span>test components</span>
+			<span>index.components</span>
 		</head-bar>
 		<div id="navLayer">
 			<div class="logos text-center">
